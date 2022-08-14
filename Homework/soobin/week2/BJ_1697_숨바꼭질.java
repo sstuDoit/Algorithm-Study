977dc2f071304286ee6aae8124e7585a18d59d0c
xpeach1510 <xpeach1510@gmail.com>
2022-08-15 01:03:45 +0900

    코드리뷰 수정

--- Homework/soobin/week2/BJ_1697_숨바꼭질.java
+++ Homework/soobin/week2/BJ_1697_숨바꼭질.java
@@ -11,26 +11,31 @@ public class BJ_1697_숨바꼭질 {
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		
-		int[] dp = new int[100002];
+		int[] list = new int[100002];
 		
-		dp[n] = 0;
-		dp[n+1] = 1;
 		
-		// 뒤로 가는 경우 기록
-		for (int i = n; i > 1; i--) dp[i-1] = dp[i] + 1;
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(n);
+		list[n] = 1;
 		
-		for (int i = n+1; i <= 100000; i++) {
-			// X2 이동 경우의 수. i / 2가 짝수라면 이전 값에서 하나 더해오는 것과 순간이동하는 것 중 더 적은 경우의 수
-			if (i % 2 == 0) dp[i] = Math.min(dp[i-1] + 1, dp[i/2] + 1);
-			else dp[i] = dp[i-1] + 1;
+		while (list[k] == 0) {
+			int cur = queue.poll(); // 큐의 값 뽑기
 			
-			// 뒤로 가는 경우 탐색. temp 위치의 값 + 1이 temp-1의 값보다 작으면 갱신
-			int temp = i;
-			while (temp > 0 && dp[temp-1] >= dp[temp] + 1) {
-				dp[temp-1] = dp[temp] + 1;
-				temp--;
+			for (int i = 0; i < 3; i++) {
+				int next = 0;
+				
+				if (i == 0) next = cur + 1;
+				else if (i == 1) next = cur - 1;
+				else next = cur * 2;
+
+				
+				if (next>= 0 && next < list.length && list[next] == 0) { // 범위 내에 있고 값이 정해지지 않았다면
+					queue.add(next); // 큐에 값 추가
+					list[next] = list[cur] + 1;
+				}
 			}
 		}
-		System.out.println(dp[k]);
+		
+		System.out.println(list[k] - 1);
 	}
 }
